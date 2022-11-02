3833728c0c22537ac2d9852b74bdca6463b73428
Dimas Suárez Rodríguez <108862116+dimassuarez@users.noreply.github.com>
2022-11-02 18:53:58 +0100

    Add files via upload

--- example/run_example.sh
+++ example/run_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-env MOL_MASK=":1" REFTOP="$PWD/top_traj_files/drug_25.top" \
-    TRAJDIR="$PWD/top_traj_files" ALIAS_MDCRD="drug_25"  \
+env NPROCS=6 MOL_MASK=":1" REFTOP="$PWD/top_traj_files/drug_25.top" \
+    TRAJDIR="$PWD/top_traj_files" MDCRD_PREFIX="drug_25" MDCRD_SUFFIX=".mdcrd" \
     DO_CC_MLA=1  CUTOFF=" -1 "  \
     SCRATCH="/scratch" ../calc_sconform.sh
 
