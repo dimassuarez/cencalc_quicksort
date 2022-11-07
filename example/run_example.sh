6bd7a6dcd1466f98a60e197dd7d3b81d19397e73
Dimas Suárez Rodríguez <108862116+dimassuarez@users.noreply.github.com>
2022-11-07 15:19:30 +0100

    Add files via upload

--- example/run_example.sh
+++ example/run_example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# make sure that calc_sconform.sh adn get_tor.py have execution permissions
+
 env NPROCS=6 MOL_MASK=":1" REFTOP="$PWD/top_traj_files/drug_25.top" \
     TRAJDIR="$PWD/top_traj_files" MDCRD_PREFIX="drug_25" MDCRD_SUFFIX=".mdcrd" \
     DO_CC_MLA=1  CUTOFF=" -1 "  \
